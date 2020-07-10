f4e213860736f283e54c1ec3131e4545fdd71426
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-07-11 01:08:13 +0300

    Update index.ts
    Minor edits

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,11 @@ declare global {
 }
 
 interface Ioptions {
-    min_value: number
-    max_value: number
-    current_value: number
+    min_value?: number
+    max_value?: number
+    current_value?: number
+    step?: number
+    show_window_value?: boolean
 }
 
 (function($) {
@@ -21,12 +23,20 @@ interface Ioptions {
         const newOptions = $.extend({
             min_value: 0,
             max_value: 100,
-            current_value: 0
+            current_value: 0,
+            step: 1,
+            show_window_value: false
         }, options)
         return this.add(new Presenter(this, newOptions).init())
     }
 })($)
 
 $(document).ready(() => {
-    $('#test').slider()
+    $('#test').slider({
+        min_value: - 1000,
+        max_value: 1000,
+        current_value: 0,
+        step: 5,
+        show_window_value: true
+    })
 })
